b400b5e4253a10152084fc6e4ad5f3fe01aedb95
1ITS-master\itsadmin <p.brundu@itsrizzoli.it>
2017-12-19 17:53:30 +0100

    ciao

--- Activity/AppMedica/app/src/main/java/com/example/itsadmin/appmedica/Registrati.java
+++ Activity/AppMedica/app/src/main/java/com/example/itsadmin/appmedica/Registrati.java
@@ -2,6 +2,10 @@ package com.example.itsadmin.appmedica;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Registrati extends Activity {
 
@@ -9,5 +13,15 @@ public class Registrati extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrati);
+        final TextView titolo=(TextView)findViewById(R.id.id_titolo);
+        final EditText nome=(EditText)findViewById(R.id.id_nome);
+        final EditText cognome=(EditText)findViewById(R.id.id_cognome);
+        final EditText email=(EditText)findViewById(R.id.id_email);
+        final EditText password=(EditText)findViewById(R.id.id_password);
+        final EditText conferma=(EditText)findViewById(R.id.id_conferma);
+        final RadioButton autorizzo=(RadioButton)findViewById(R.id.id_autorizzo);
+        final Button button1=(Button)findViewById(R.id.id_button1);
+        final TextView haigia=(TextView)findViewById(R.id.id_hai_gia);
+
     }
 }
