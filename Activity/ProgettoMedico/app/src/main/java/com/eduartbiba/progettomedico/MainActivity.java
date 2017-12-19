cf688ddf9d3a67c3ff6fbbc1d5a82a6c137e7907
1ITS-master\itsadmin <ciao>
2017-12-19 17:54:03 +0100

    no message

--- Activity/ProgettoMedico/app/src/main/java/com/eduartbiba/progettomedico/MainActivity.java
+++ Activity/ProgettoMedico/app/src/main/java/com/eduartbiba/progettomedico/MainActivity.java
@@ -2,6 +2,7 @@ package com.eduartbiba.progettomedico;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView login = (TextView) findViewById(R.id.login);
     }
 }
