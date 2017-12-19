5d82f2e74808395a312125ee6ca6ee3787412527
1ITS-master\itsadmin <eduart1994@libero.it>
2017-12-19 17:12:26 +0100

    + recupera passwrd

---
+++ ProgettoMedico/app/src/main/java/com/eduartbiba/progettomedico/RecuperaPassword.java
@@ -0,0 +1,13 @@
+package com.eduartbiba.progettomedico;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecuperaPassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recupera_password);
+    }
+}
