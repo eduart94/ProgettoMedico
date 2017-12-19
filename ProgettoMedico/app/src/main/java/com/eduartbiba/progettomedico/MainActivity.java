b5d2015c38ff569d8974f75b714514bec24d8dad
1ITS-master\itsadmin <eduart1994@libero.it>
2017-12-19 16:48:27 +0100

    modifiche

--- ProgettoMedico/app/src/main/java/com/eduartbiba/progettomedico/MainActivity.java
+++ ProgettoMedico/app/src/main/java/com/eduartbiba/progettomedico/MainActivity.java
@@ -2,6 +2,9 @@ package com.eduartbiba.progettomedico;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView lgn = (TextView)findViewById(R.id.login);
+        TextView Email = (TextView)findViewById(R.id.email);
+        EditText insem = (EditText)findViewById(R.id.insemail);
+        TextView pass = (TextView)findViewById(R.id.password);
+        EditText inspass = (EditText)findViewById(R.id.inspassword);
+        Button passdim = (Button)findViewById(R.id.passdimenticata);
+        Button nonreg = (Button)findViewById(R.id.nonregistrato);
+        Button acced = (Button)findViewById(R.id.accedi);
+
+
+
     }
 }
