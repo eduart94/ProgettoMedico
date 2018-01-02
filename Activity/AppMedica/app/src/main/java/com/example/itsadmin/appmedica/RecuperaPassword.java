609767e21d7398973a1b75485f746cca7e03c59e
eduart94 <eduart1994@libero.it>
2018-01-02 18:45:30 +0100

    aggiunto recuperaPassword e login

---
+++ Activity/AppMedica/app/src/main/java/com/example/itsadmin/appmedica/RecuperaPassword.java
@@ -0,0 +1,13 @@
+package com.example.itsadmin.appmedica;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecuperaPassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recupera_password);
+    }
+}
