b9aae2e0557c255fa641add5cb00e242a60c01c5
1ITS-master\itsadmin <eduart1994@libero.it>
2018-01-09 17:56:59 +0100

    intent recupera password

--- Activity/AppMedica/app/src/main/java/com/example/itsadmin/appmedica/Login.java
+++ Activity/AppMedica/app/src/main/java/com/example/itsadmin/appmedica/Login.java
@@ -1,67 +1,26 @@
 package com.example.itsadmin.appmedica;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends Activity {
-
-    public class MainActivity1 extends Activity {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_recupera_password);
-            Button button2=(Button)findViewById(R.id.recuperapassword);
-            button2.setOnClickListener(new OnClickListener(){
-                @Override
-                public void onClick(View arg0) {
-                    // definisco l'intenzione
-                    Intent Recpass = new Intent(MainActivity1.this,RecuperaPassword.class);
-                    // passo all'attivazione dell'activity Recpass.java
-                    startActivity(Recpass);
-                }
-            });
-        }
-    }
-
-    public class MainActivity2 extends Activity {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_registrati);
-            Button Button3=(Button)findViewById(R.id.noregistrazione);
-            Button3.setOnClickListener(new OnClickListener(){
-                @Override
-                public void onClick(View arg0) {
-                    // definisco l'intenzione
-                    Intent NoReg = new Intent(MainActivity2.this,Registrati.class);
-                    // passo all'attivazione dell'activity Pagina.java
-                    startActivity(NoReg);
-                }
-            });
-        }
-    }
-
-    public class MainActivity3 extends Activity {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
-            Button login=(Button)findViewById(R.id.accedi);
-            login.setOnClickListener(new OnClickListener(){
-                @Override
-                public void onClick(View arg0) {
-                    // definisco l'intenzione
-                    Intent Profile = new Intent(MainActivity3.this,Profilo.class);
-                    // passo all'attivazione dell'activity Pagina.java
-                    startActivity(Profile);
-                }
-            });
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button haigiaaccount = findViewById(R.id.haigiaaccount);
+        haigiaaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent recupPasswordIntent = new Intent(Login.this, RecuperaPassword.class);
+                startActivity(recupPasswordIntent);
+            }
+        });
+
     }
 }
