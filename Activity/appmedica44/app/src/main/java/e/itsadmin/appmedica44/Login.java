e642a7de4d14c3e6d8cf0162d24ee6fd10d7d605
1ITS-master\itsadmin <ciao>
2018-01-10 16:55:15 +0100

    no message

---
+++ Activity/appmedica44/app/src/main/java/e/itsadmin/appmedica44/Login.java
@@ -0,0 +1,63 @@
+package e.itsadmin.appmedica44;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final TextView login=(TextView)findViewById(R.id.login);
+        final TextView email=(TextView) findViewById(R.id.email);
+        final TextView password=(TextView)findViewById(R.id.password);
+        final Button passLost=(Button)findViewById(R.id.passlost);
+        final Button NoReg=(Button)findViewById(R.id.noregistrazione);
+        final Button Accesso=(Button)findViewById(R.id.accedi);
+        final EditText InsEmail=(EditText)findViewById(R.id.insmail);
+        final EditText InsPass=(EditText)findViewById(R.id.inspass);
+
+        Button button2=(Button)findViewById(R.id.passlost);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // definisco l'intenzione
+                Intent recpass = new Intent(Login.this,RecuperaPassword.class);
+                // passo all'attivazione dell'activity Recpass.java
+                startActivity(recpass);
+            }
+
+            });
+
+        Button Button3=(Button)findViewById(R.id.noregistrazione);
+        Button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // definisco l'intenzione
+                Intent noReg = new Intent(Login.this,Registrati.class);
+                // passo all'attivazione dell'activity Pagina.java
+                startActivity(noReg);
+            }
+        });
+
+
+
+            Button accedi=(Button)findViewById(R.id.accedi);
+            accedi.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    // definisco l'intenzione
+                    Intent profile = new Intent(Login.this,Profilo.class);
+                    // passo all'attivazione dell'activity Pagina.java
+                    startActivity(profile);
+                }
+            });}
+    }
+
