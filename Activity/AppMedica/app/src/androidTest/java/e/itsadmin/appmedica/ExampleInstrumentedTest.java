e620d397392b0f64b04656e13f227922f015bb75
1ITS-master\itsadmin <eduart1994@libero.it>
2018-01-10 15:47:35 +0100

    tr

--- Activity/AppMedica/app/src/androidTest/java/e/itsadmin/appmedica/ExampleInstrumentedTest.java
+++ Activity/AppMedica/app/src/androidTest/java/e/itsadmin/appmedica/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.itsadmin.appmedica;
+package e.itsadmin.appmedica;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.itsadmin.appmedica", appContext.getPackageName());
+        assertEquals("e.itsadmin.appmedica", appContext.getPackageName());
     }
 }
