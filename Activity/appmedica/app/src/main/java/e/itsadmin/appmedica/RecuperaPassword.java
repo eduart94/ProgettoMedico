bbfef17db9af083ecb757e6ab76f08c171f461b3
1ITS-master\itsadmin <ciao>
2018-01-10 16:34:17 +0100

    no message

---
+++ Activity/appmedica/app/src/main/java/e/itsadmin/appmedica/RecuperaPassword.java
@@ -0,0 +1,35 @@
+package e.itsadmin.appmedica;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RecuperaPassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recupera_password);
+        final EditText Email=(EditText)findViewById(R.id.mailmodifica);
+        Button but =(Button)findViewById(R.id.invia);
+
+
+        Button button=(Button)findViewById(R.id.invia);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // definisco l'intenzione
+
+                Intent button = new Intent(RecuperaPassword.this,Login.class);
+                // passo all'attivazione dell'activity Recpass.java
+                startActivity(button);
+                Toast.makeText(RecuperaPassword.this,"Ti abbiamo inviato un'email con le informazioni di recupero password",Toast.LENGTH_LONG).show();
+
+    }
+});
+    }
+}
---
+++ Activity/appmedica/app/src/main/java/e/itsadmin/appmedica/Registrati.java
@@ -0,0 +1,33 @@
+package e.itsadmin.appmedica;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class Registrati extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrati);
+        final TextView titolo=(TextView)findViewById(R.id.id_titolo);
+        final EditText nome=(EditText)findViewById(R.id.id_nome);
+        final EditText cognome=(EditText)findViewById(R.id.id_cognome);
+        final EditText email=(EditText)findViewById(R.id.id_email);
+        final EditText password=(EditText)findViewById(R.id.id_password);
+        final EditText conferma=(EditText)findViewById(R.id.id_conferma);
+        final RadioButton autorizzo=(RadioButton)findViewById(R.id.id_autorizzo);
+        final Button button1=(Button)findViewById(R.id.id_button1);
+        final TextView haigiaaccount=(TextView)findViewById(R.id.id_password);
+
+
+
+    }
+
+
+}
