678c5bf6693629e89459815850f80ef4980c5118
1ITS-master\itsadmin <ciao>
2018-02-28 12:06:18 +0100

    modifiche davide

---
+++ JAVA eclipse/ProgettoMedicoWeb/WebContent/js/caledisp.js
@@ -0,0 +1,10 @@
+$(function () {
+    $( '#table' ).searchable({
+        striped: true,
+        oddRow: { 'background-color': '#f5f5f5' },
+        evenRow: { 'background-color': '#fff' },
+        searchType: 'fuzzy'
+    });
+    
+
+});
