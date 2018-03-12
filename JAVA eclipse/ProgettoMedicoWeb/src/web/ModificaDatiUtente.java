9242eaeda443871b243d1807ecc89cb1f3e4392e
eddy <nb>
2018-03-12 09:55:03 +0100

    piccole modifiche

---
+++ JAVA eclipse/ProgettoMedicoWeb/src/web/ModificaDatiUtente.java
@@ -0,0 +1,39 @@
+package web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import gestione.EsitoOperazione;
+import gestione.GestioneAccountUtente;
+
+/**
+ * Servlet implementation class ModificaDatiUtente
+ */
+@WebServlet("/ModificaDatiUtente")
+public class ModificaDatiUtente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private GestioneAccountUtente gau = new GestioneAccountUtente();
+	private ObjectMapper mapper = new ObjectMapper();
+    
+    public ModificaDatiUtente() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String password = request.getParameter("passwordNuova");
+		
+		
+		
+		
+	}
+
+}
