625c03131dfbad8045cb91277a36098af6351236
eddy <nb>
2018-03-12 11:31:13 +0100

    creato servlet modifica dati per utente e medico e creato metodo di aggiornamento di password e numero di telefono

--- JAVA eclipse/ProgettoMedicoWeb/src/web/ModificaDatiMedico.java
+++ JAVA eclipse/ProgettoMedicoWeb/src/web/ModificaDatiMedico.java
@@ -10,35 +10,33 @@ import javax.servlet.http.HttpServletResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gestione.EsitoOperazione;
-import gestione.GestioneAccountUtente;
+import gestione.GestioneAccountMedico;
 
 /**
- * Servlet implementation class ModificaDatiUtente
+ * Servlet implementation class ModificaDatiMedico
  */
-@WebServlet("/ModificaDatiUtente")
-public class ModificaDatiUtente extends HttpServlet {
+@WebServlet("/ModificaDatiMedico")
+public class ModificaDatiMedico extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	private GestioneAccountUtente gau = new GestioneAccountUtente();
-	private ObjectMapper mapper = new ObjectMapper();
-    
-    public ModificaDatiUtente() {
+       private GestioneAccountMedico gam = new GestioneAccountMedico();
+       private ObjectMapper mapper = new ObjectMapper();
+  
+    public ModificaDatiMedico() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 		String numeroTelefono = request.getParameter("numeroTelefono");
-		EsitoOperazione eo = gau.aggiornaPasswordUtente(email, password, numeroTelefono);
+		
+		EsitoOperazione eo = gam.aggiornaDatiMedico(email, password, numeroTelefono);
 		
 		String json = mapper.writeValueAsString(eo);
 		response.setContentType("application/json");
 		response.getWriter().append(json);
-		
-		
 		
 	}
 
